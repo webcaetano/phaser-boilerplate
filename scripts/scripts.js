f68bbb5c31e62d0fc4dcdc85c83e72970d950e03
Andre Caetano <webandrecaetano@gmail.com>
2016-11-13 03:18:23 -0200

    Deploy to Github Pages

---
+++ scripts/scripts.js
@@ -0,0 +1,1 @@
+!function(t){function r(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return t[o].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var e={};return r.m=t,r.c=e,r.p="",r(0)}([function(t,r,e){"use strict";var o=e(1);console.log(o)},function(t,r){"use strict";t.exports="test"}]);
